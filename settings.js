ae242a49748e7d7dd317b939d248ded7d71065e0
Silvana <silvana.vieira@hotmail.com>
2020-03-08 15:10:42 -0300

    configs do meu vscode.. nao manter no projeto

---
+++ settings.js
@@ -0,0 +1,108 @@
+
+//configs do meu vscode.. nao manter no projeto
+{
+  "editor.wordWrap": "wordWrapColumn",
+    "editor.wrappingIndent": "same",
+      "editor.wordWrapColumn": 80,
+        "editor.codeActionsOnSave": {
+    "source.fixAll.eslint": true
+  },
+  "editor.formatOnSave": true,
+    "window.zoomLevel": -2,
+      "[javascript]": {
+    "editor.defaultFormatter": "vscode.typescript-language-features"
+  },
+  "[html]": {
+    "editor.defaultFormatter": "vscode.html-language-features"
+  },
+  "liveServer.settings.donotShowInfoMsg": true,
+    "emmet.includeLanguages": {
+    "javascript": "javascriptreact"
+  },
+  "javascript.updateImportsOnFileMove.enabled": "always",
+    "editor.rulers": [
+      80
+    ],
+      "workbench.colorCustomizations": {
+    "editorRuler.foreground": "#ff4081"
+  },
+  "[javascriptreact]": {
+    "editor.defaultFormatter": "vscode.typescript-language-features"
+  },
+  "javascript.preferences.quoteStyle": "single",
+    "typescript.preferences.quoteStyle": "single",
+      "prettier.jsxSingleQuote": true,
+        "prettier.singleQuote": true,
+          "html.format.wrapAttributes": "force",
+            "html.format.wrapLineLength": 40,
+              "path-intellisense.absolutePathToWorkspace": false,
+                "editor.lineHeight": 0,
+                  "editor.fontSize": 16,
+                    "editor.fontFamily": "Fira Code",
+                      "editor.fontLigatures": true,
+                        "peacock.favoriteColors": [
+                          {
+                            "name": "Angular Red",
+                            "value": "#b52e31"
+                          },
+                          {
+                            "name": "Auth0 Orange",
+                            "value": "#eb5424"
+                          },
+                          {
+                            "name": "Azure Blue",
+                            "value": "#007fff"
+                          },
+                          {
+                            "name": "C# Purple",
+                            "value": "#68217A"
+                          },
+                          {
+                            "name": "Gatsby Purple",
+                            "value": "#639"
+                          },
+                          {
+                            "name": "Go Cyan",
+                            "value": "#5dc9e2"
+                          },
+                          {
+                            "name": "Java Blue-Gray",
+                            "value": "#557c9b"
+                          },
+                          {
+                            "name": "JavaScript Yellow",
+                            "value": "#f9e64f"
+                          },
+                          {
+                            "name": "Mandalorian Blue",
+                            "value": "#1857a4"
+                          },
+                          {
+                            "name": "Node Green",
+                            "value": "#215732"
+                          },
+                          {
+                            "name": "React Blue",
+                            "value": "#00b3e6"
+                          },
+                          {
+                            "name": "Something Different",
+                            "value": "#832561"
+                          },
+                          {
+                            "name": "Vue Green",
+                            "value": "#42b883"
+                          }
+                        ],
+                          "sync.gist": "fea6f5cb6f7a1810a31da0c41c319a51",
+                            "sync.autoDownload": true,
+                              "sync.autoUpload": true,
+                                "sync.forceDownload": true,
+                                  "sync.forceUpload": true,
+                                    "sync.quietSync": false,
+                                      "workbench.iconTheme": "material-icon-theme",
+                                        "workbench.colorTheme": "Material Theme Darker High Contrast",
+                                          "editor.largeFileOptimizations": false,
+                                            "terminal.integrated.fontSize": 12,
+                                              "files.autoSave": "off",
+}
